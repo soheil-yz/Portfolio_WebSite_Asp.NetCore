9ed341efa234547f7d121dab0785ac7d7474331c
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2023-09-30 11:15:16 +0330

    .

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,14 @@
 ﻿using DevCart.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace DevCart.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
@@ -23,7 +16,7 @@ namespace DevCart.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
