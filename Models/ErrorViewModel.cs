f5d5fe718f495a16a9d3cde9a77159dc1a468df7
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2023-09-30 09:58:21 +0330

    .

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DevCart.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
