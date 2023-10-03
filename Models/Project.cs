70c7f7754ee7c87645892e5e88b8192c5352a6d0
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2023-10-03 18:06:24 +0330

    .

--- Models/Project.cs
+++ Models/Project.cs
@@ -2,10 +2,20 @@
 {
 	public class Project
 	{
-		public string Id { get; set; }
+		public long Id { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
+		public string Image { get; set; }
 		public string Clinet {  get; set; }
 
+		public Project(long id, string name, string description,string image, string clinet)
+		{
+			Id = id;
+			Name = name;
+			Description = description;
+			Image = image;
+			Clinet = clinet;
+		}
 	}
+
 }
--- ViewComponents/ProjectViewComponent.cs
+++ ViewComponents/ProjectViewComponent.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevCart.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 namespace DevCart.ViewComponents
 {
@@ -6,7 +9,14 @@ namespace DevCart.ViewComponents
 	{
 		public IViewComponentResult Invoke()
 		{
-			return View("_Projects");
+			var projects = new List<Project>
+			{
+				new Project(2,"Project","Lorem ipsum dolor sit amet, cagna aliqua","Project-2.jpg","soheil2"),
+				new Project(1,"Project","Lorem ipsum dolor sit amet, cagna aliqua","Project-1.jpg","soheil1"),
+				new Project(3,"Project","Lorem ipsum dolor sit amet, cagna aliqua","Project-3.jpg","soheil3"),
+				new Project(4,"Project","Lorem ipsum dolor sit amet, cagna aliqua","Project-4.jpg","soheil4")
+			};
+			return View("_Projects",projects);
 		}
 	}
 }
