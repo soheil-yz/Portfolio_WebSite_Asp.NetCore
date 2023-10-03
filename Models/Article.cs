d8cab7258637afd3d915ac283ea00ab8a0cb91e4
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2023-10-03 22:17:42 +0330

    .

--- Models/Article.cs
+++ Models/Article.cs
@@ -5,5 +5,14 @@
 		public int Id { get; set; }
 		public string Title { get; set; }
 		public string Description { get; set; }
+		public string Image { get; set; }
+
+		public Article(int id, string title, string description, string image)
+		{
+			Id = id;
+			Title = title;
+			Description = description;
+			Image = image;
+		}
 	}
 }
---
+++ ViewComponents/LatestArticalesViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using DevCart.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DevCart.ViewComponents
+{
+	public class LatestArticalesViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var articales = new List<Article>
+			{
+				new Article(1,"Project","Lorem ipsum dolor sit amet, consectetur adipiscing elit","blog-post-thumb-card-1.jpg"),
+				new Article(2,"Project","Lorem ipsum dolor sit amet, consectetur adipiscing elit","blog-post-thumb-card-2.jpg"),
+				new Article(3,"Project","Lorem ipsum dolor sit amet, consectetur adipiscing elit","blog-post-thumb-card-3.jpg"),
+				new Article(4,"Project","Lorem ipsum dolor sit amet, consectetur adipiscing elit","blog-post-thumb-card-4.jpg") };
+			return View("_LatestArticales",articales);
+		}
+	}
+}
