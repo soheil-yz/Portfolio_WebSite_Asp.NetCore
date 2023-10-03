8913097548e1a3ef3f9cf7c61691852db97be649
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2023-10-03 10:32:33 +0330

    .

---
+++ Models/Article.cs
@@ -0,0 +1,9 @@
+﻿namespace DevCart.Models
+{
+	public class Article
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+	}
+}
---
+++ Models/Project.cs
@@ -0,0 +1,11 @@
+﻿namespace DevCart.Models
+{
+	public class Project
+	{
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string Clinet {  get; set; }
+
+	}
+}
