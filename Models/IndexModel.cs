10b857158784df0dd6ee0f7b5ded327d48f24d2f
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2023-10-03 13:33:36 +0330

    .

---
+++ Models/IndexModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DevCart.Models
+{
+	public class IndexModel
+	{
+		public List<Project> Projects { get; set; }
+		public List<Article> Articles { get; set; }
+	}
+}
